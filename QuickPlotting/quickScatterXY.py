a9ac19d96e3f0b8e7065dcf61443362a12342607
hherde <hherde@brandeis.edu>
2017-03-29 23:17:12 -0400

    Developing the quickScatterXY features

---
+++ QuickPlotting/quickScatterXY.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import shutil, os, sys 
+
+def runStats( vals ):
+  ar = np.asarray( vals ).astype( np.float )
+  Max = np.amax( ar )
+  Min = np.amin( ar )
+  Range = Max - Min
+  Mean = np.mean( ar )
+  RMS = np.std( ar )
+  return Max, Min, Range, Mean, RMS
+
+def statReport( vals ):
+  stats = runStats( vals )
+  print ' Max: %.6f \n Min: %.6f \n Range: %.6f \n Mean: %.6f \n RMS: %.6f \n' % ( stats[ 0 ], stats[ 1 ], stats[ 2 ], stats[ 3 ], stats[ 4 ] )
+
+if sys.platform == 'ios': 
+  import console, appex
+  console.clear()
+  File = appex.get_file_path()
+elif sys.platform == 'darwin': 
+  from optparse import OptionParser
+  parser = OptionParser()
+  parser.add_option( '-f', '--filename', dest = 'filename', action = 'store', type = 'string', default = 'enabled.csv', help = 'Supply filename' )
+  ( options, args ) = parser.parse_args()
+  File = options.filename
+else: 
+  from optparse import OptionParser
+  parser = OptionParser()
+  parser.add_option( '-f', '--filename', dest = 'filename', action = 'store', type = 'string', default = 'enabled.csv', help = 'Supply filename' )
+  ( options, args ) = parser.parse_args()
+  File = options.filename
+
+print( 'Input path: %s \n' % File )
+with open( File, 'r' ) as f: 
+  print 'First line reads: ' + f.readline().strip()
+
+ts = raw_input( 'Analyze time stamp? (y/n)    ' ) 
+if ts == 'y' or ts == 'yes' or ts == 'Yes':
+  useTime = True
+  lineindex=0
+  timestamp = []
+  for line in open( File, 'r' ):
+    vals = line.split( ',' )
+    Hour = str( int( float( vals[ 0 ] ) ) )
+    Minute = str( int( float( vals[ 1 ] ) ) )
+    Second = str( float( float( vals[ 2 ] ) ) + float( vals[ 3 ] ) )
+    time = float( Hour )*60*60 + float( Minute )*60 + float( Second )
+    if lineindex == 0: starttime = time
+    time = time-starttime
+    lineindex = lineindex + 1
+    timestamp.append( time ) 
+  print 'Independent variable set to timestamp. \nTimestamp fills indices 0-3.\n'
+  
+else:  
+  useTime = False
+  X = int( raw_input( 'Independent variable column index: ' ) )
+Y = int( raw_input( 'Dependent variable column index: ' ) )
+
+if not useTime: valX = []
+valY = []
+
+for line in open( File, 'r' ):
+	l = line.split( ',' )
+	if not useTime: valX.append( l[ X ] )
+	valY.append( l[ Y ] )
+
+print '\nDependent variable analysis: \n'
+statReport( valY ) 
+print '\nPreparing plot...'
+
+if useTime: plt.plot( timestamp, valY, 'ro' )
+else: plt.plot( valX, valY, 'ro' )
+plt.show() 
